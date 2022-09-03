f7aa7c9430d72f09d9621a598e9bca26e1a9fd0e
seco <sercanpaspal@gmail.com>
2022-09-03 20:37:15 +0300

    feat: down fps to 60

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 let player
-const FPS = 120;
+const FPS = 60;
 const INPUTS = {};
 let NODES = [];
 let MOUSE = { x: 0, y: 0 }
